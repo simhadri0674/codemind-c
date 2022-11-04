71528e08e4552d28afabf30790a051907efc7435
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-11-04 11:10:28 +0530

    #include <bits/stdc++.h> using namespace std; int main(){     int a,b,c;     cin>>a;     c=0;     b=a;     for (int i=0;i<b;i++){         c +=i;
        }
        cout<<c;
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.c
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a;
+    c=0;
+    b=a;
+    for (int i=0;i<b;i++){
+        c +=i;
+        
+    }
+    cout<<c;
+}
