c40911e8d150a3e3ab1f98a4f5df998aa6e46050
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-11-05 16:53:32 +0530

    #include <bits/stdc++.h> using namespace std; int main (){     int a,b,c;     cin>>a;     for (int i=a;i>=1;i--){         for (int j=1;j<=i;j++){             cout<<j;         }         cout<<endl;     }     return 0;
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,9 +3,9 @@ using namespace std;
 int main (){
     int a,b,c;
     cin>>a;
-    for (int i=1;i<=a;i++){
-        for (int j=a;j>=1;j--){
-            cout<<j<<" ";
+    for (int i=a;i>=1;i--){
+        for (int j=1;j<=i;j++){
+            cout<<j;
         }
         cout<<endl;
     }
