e9c1df8d907f2043092e03ea672a32a14bc3d6cb
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-11-05 21:19:20 +0530

    #include <bits/stdc++.h> using namespace std; int main(){     int a,b,c,d,r;     cin>>a;     b=sqrt(a);     if (a%b==0){         cout<<"True";     }     else cout<<"False"; }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c,d,r;
+    cin>>a;
+    b=sqrt(a);
+    if (a%b==0){
+        cout<<"True";
+    }
+    else cout<<"False";
+}
