2894cb783c9aaf347244ff261e4e12216a010e4b
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-11-05 10:44:26 +0530

    #include <bits/stdc++.h> using namespace std; int main (){     int a,b,c;     cin>>a>>b>>c;     int d,e,f,g;     d=0;     d=pow(a,b);     e=0;     e=d%c;     cout<<e;
    }

---
+++ Power_of_a_Number.c
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d,e,f,g;
+    d=0;
+    d=pow(a,b);
+    e=0;
+    e=d%c;
+    cout<<e;
+    
+}
