e9faa309829bacdd64b4934468cb3523d29af76b
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-11-05 16:42:16 +0530

    #include <bits/stdc++.h> using namespace std; int main (){     int a,b,c;     cin>>a;     for (int i=1;i<=a;i++){         for (int j=1;j<=a;j++){             cout<<i<<" ";         }         // cout << ' ';         cout << endl;     }     return 0;
    }

---
+++ Pattern.c
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    int a,b,c;
+    cin>>a;
+    for (int i=1;i<=a;i++){
+        for (int j=1;j<=a;j++){
+            cout<<i<<" ";
+        }
+        // cout << '
+';
+        cout << endl;
+    }
+    return 0;
+    
+}
